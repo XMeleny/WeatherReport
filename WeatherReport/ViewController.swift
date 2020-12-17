7af6142c324ef052a222856eb1246e54a7bb714a
zhuxiaomei <865622793@qq.com>
2020-12-17 17:47:15 +0800

    complete query weather using api

--- WeatherReport/ViewController.swift
+++ WeatherReport/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
     
     var locationManager:CLLocationManager!
@@ -22,20 +22,21 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }()
     
     var currentAnnotation:MyAnnotation?
+    var currentCity:String?
+    var currentAdcode:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //mapView settings
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .followWithHeading
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        determineCurrentLocation()
+        initLocation()
     }
     
-    func determineCurrentLocation(){
+    func initLocation(){
         locationManager = CLLocationManager()
         locationManager.delegate=self
         locationManager.desiredAccuracy=kCLLocationAccuracyBest
@@ -51,50 +52,101 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let latitude = userLocation.coordinate.latitude
         let longitude = userLocation.coordinate.longitude
         let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.75, longitudeDelta: 0.75))
         mapView.setRegion(region, animated: true)
         
-        updateCityInLabel(latitude: latitude, longitude: longitude)
+        getCityInformation(latitude: latitude, longitude: longitude)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("error.localizedDescription = \(error.localizedDescription)")
     }
     
-    func updateCityInLabel(latitude:CLLocationDegrees, longitude:CLLocationDegrees){
-        let location = CLLocation(latitude: latitude, longitude: longitude)
-        geoCoder.reverseGeocodeLocation(location) {
-            (placemarks, error) -> Void in
-            if let placeMark = placemarks {
-                self.positionLabel.text=placeMark[0].locality
-            }
-        }
-    }
-    
-    //todo
-    func updateWeather(city:String){
-        
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         let point = touches.first?.location(in: mapView)
         let coordinate = mapView.convert(point!, toCoordinateFrom: mapView)
         
-        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
-        geoCoder.reverseGeocodeLocation(location) {
-            (pls: [CLPlacemark]?, error: Error?) -> Void in
-            if error == nil {
-                if let pl = pls?.first {
-                    let annotation = MyAnnotation(coordinate,pl.locality,pl.name)
-                    self.mapView.addAnnotation(annotation)
-                    if self.currentAnnotation != nil{
-                        self.mapView.removeAnnotation(self.currentAnnotation!)
-                    }
-                 
-                    self.currentAnnotation = annotation
-                    self.positionLabel.text = pl.locality
+        getCityInformation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+    
+    let SCHEME_AND_HOST = "https://restapi.amap.com"
+    let PATH_CITY_INFORMATION = "/v3/geocode/regeo"
+    let PATH_WEATHER_INFORMATION = "/v3/weather/weatherInfo"
+    let PARAM_KEY = "key=9fd87874bd0d22ba54679a8571db1974"
+    let PARAM_EXTENSIONS = "extensions=all"
+    
+    func getCityInformation(latitude:Double, longitude:Double){
+        let url = URL(string: "\(SCHEME_AND_HOST)\(PATH_CITY_INFORMATION)?\(PARAM_KEY)&location=\(longitude),\(latitude)")!
+        let task = URLSession.shared.dataTask(with: url){ (data,response,error) in
+            let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]
+            let info = json?["info"] as? String
+            
+            if info == "OK"{
+                let regeocode = json!["regeocode"] as! [String:Any]
+                let addressComponent = regeocode["addressComponent"] as! [String:Any]
+                let provice = addressComponent["province"] as! String
+                let city = addressComponent["city"] as? String
+                let district = addressComponent["district"] as? String
+                let adcode = addressComponent["adcode"] as! String
+                
+                var realCity = city
+                if city == nil {
+                    realCity = provice
                 }
+                
+                self.updateCityLabelInMainThread(city: realCity!)
+                
+                self.updataAnnotationInMainThread(
+                    coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+                    city: realCity!,
+                    district: district!
+                )
+                self.getWeatherInformation(adcode: adcode)
             }
+        }
+        task.resume()
+    }
+
+    func getWeatherInformation(adcode:String){
+        let url = URL(string: "\(SCHEME_AND_HOST)\(PATH_WEATHER_INFORMATION)?\(PARAM_KEY)&\(PARAM_EXTENSIONS)&city=\(adcode)")!
+        
+        let task = URLSession.shared.dataTask(with: url){ (data,response,error) in
+            let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]
+
+            let infocode = json!["infocode"] as? String
+            if infocode == "10000" {
+                let forecasts = json!["forecasts"] as? NSArray
+                let casts = (forecasts?[0] as? [String:Any])?["casts"] as? NSArray
+                let tomorrowWeather = casts?[1] as? [String:Any]
+                let tomorrowDayWeather = tomorrowWeather?["dayweather"]
+                let tomorrowNightWeather = tomorrowWeather?["nightweather"]
+                
+                self.updateWeatherLabelInMainThread(weather: "\(tomorrowDayWeather!)")
+            }
+        }
+        task.resume()
+    }
+    
+    func updateCityLabelInMainThread(city:String){
+        DispatchQueue.main.async(execute: {
+            self.cityLabel.text = city
+        })
+    }
+    
+    func updateWeatherLabelInMainThread(weather:String)  {
+        DispatchQueue.main.async {
+            self.weatherLabel.text = weather
+        }
+    }
+    
+    func updataAnnotationInMainThread(coordinate:CLLocationCoordinate2D ,city:String, district:String){
+        DispatchQueue.main.async {
+            let annotation = MyAnnotation(coordinate,city,district)
+            self.mapView.addAnnotation(annotation)
+            if self.currentAnnotation != nil {
+                self.mapView.removeAnnotation(self.currentAnnotation!)
+            }
+            self.currentAnnotation = annotation
         }
     }
 }
