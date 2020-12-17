004a824ca60b547dd5c48f6fc02ef22b81ba9115
zhuxiaomei <865622793@qq.com>
2020-12-17 19:07:46 +0800

    add loading hint

--- WeatherReport/ViewController.swift
+++ WeatherReport/ViewController.swift
@@ -93,41 +93,47 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                     realCity = provice
                 }
                 
+                self.updateCityLabelInMainThread(city: realCity!)
+                self.updataAnnotationInMainThread(
+                    coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+                    city: realCity!,
+                    district: district ?? ""
+                )
+                
                 if adcode != self.currentAdcode{
-                    self.updateCityLabelInMainThread(city: realCity!)
-                    
-                    self.updataAnnotationInMainThread(
-                        coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
-                        city: realCity!,
-                        district: district ?? ""
-                    )
                     self.getWeatherInformation(adcode: adcode)
-                    
                     self.currentAdcode = adcode
                 }
+            }else{
+                self.httpError()
             }
         }
         task.resume()
+        setLoading(label: cityLabel)
     }
-
+    
     func getWeatherInformation(adcode:String){
         let url = URL(string: "\(SCHEME_AND_HOST)\(PATH_WEATHER_INFORMATION)?\(PARAM_KEY)&\(PARAM_EXTENSIONS)&city=\(adcode)")!
         
         let task = URLSession.shared.dataTask(with: url){ (data,response,error) in
             let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]
-
+            
             let infocode = json!["infocode"] as? String
             if infocode == "10000" {
                 let forecasts = json!["forecasts"] as? NSArray
                 let casts = (forecasts?[0] as? [String:Any])?["casts"] as? NSArray
-                let tomorrowWeather = casts?[1] as? [String:Any]
-                let tomorrowDayWeather = tomorrowWeather?["dayweather"]
-                let tomorrowNightWeather = tomorrowWeather?["nightweather"]
-                
-                self.updateWeatherLabelInMainThread(weather: "\(tomorrowDayWeather!)")
+                if casts != nil && casts!.count >= 1 {
+                    let tomorrowWeather = casts?[1] as? [String:Any]
+                    let tomorrowDayWeather = tomorrowWeather?["dayweather"]
+                    
+                    self.updateWeatherLabelInMainThread(weather: "\(tomorrowDayWeather!)")
+                }
+            }else{
+                self.httpError()
             }
         }
         task.resume()
+        setLoading(label: weatherLabel)
     }
     
     func updateCityLabelInMainThread(city:String){
@@ -150,6 +156,21 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 self.mapView.removeAnnotation(self.currentAnnotation!)
             }
             self.currentAnnotation = annotation
+        }
+    }
+    
+    let errorMsg = "网络错误，请稍后重试"
+    func httpError(){
+        DispatchQueue.main.async {
+            self.cityLabel.text = self.errorMsg
+            self.weatherLabel.text = self.errorMsg
+        }
+    }
+    
+    let loadingMsg = "加载中..."
+    func setLoading(label:UILabel){
+        DispatchQueue.main.async {
+            label.text = self.loadingMsg
         }
     }
 }
